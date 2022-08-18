fad11e10857865350a9ee975f36d76e7f5b3a216
David <david.cicconi94@gmail.com>
2022-08-18 12:47:45 -0300

    rompiendo

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,13 +3,7 @@ const { DataTypes, UUIDV4 } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("videogame", {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,

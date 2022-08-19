ea98c07aa96b0e9c0d96d53726db20a25024810f
David <david.cicconi94@gmail.com>
2022-08-18 21:07:44 -0300

    api & db

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,34 +1,60 @@
 // Cargamos las variables de entorno
-const { API_KEY } = process.env;
-const { Videogame, Genres } = require("../db.js");
 const axios = require("axios");
 require("dotenv").config();
+const { Videogame, Genre } = require("../db.js");
+const { API_KEY } = process.env;
 
 // TRAER LOS 100 JUEGOS DE LA API
-const apiData = async (req, res) => {
-  const url = `https://api.rawg.io/api/games?key=${API_KEY}`;
+const apiData = async () => {
+  let url = `https://api.rawg.io/api/games?key=${API_KEY}`;
   let videogames = []; // Aca guardo los 100 juegos con las propiedades que necesito
 
-  // Como el axios me devuelve 20 juegos y yo necesito 100, recorro hasta 5 veces el array
-  for (let i = 0; i < 5; i++) {
-    const response = await axios.get(url); // Me trae un array con 20 juegos
-    let gamesList = response.data.results;
+  try {
+    // Como el axios me devuelve 20 juegos y yo necesito 100, recorro hasta 5 veces el array
+    for (let i = 0; i < 5; i++) {
+      const response = await axios.get(url); // Me trae un array con 20 juegos
+      let gamesList = response.data.results;
 
-    // vamos a mapear para adquirir las props que necesitamos
-    gamesList.map((vg) => {
-      videogames.push({
-        id: vg.id,
-        name: vg.name,
-        image: vg.background_image,
-        released: vg.released,
-        rating: vg.rating,
-        platforms: vg.platforms?.map((el) => el.platform.name),
-        genres: vg.genres?.map((el) => el.name),
+      // vamos a mapear para adquirir las props que necesitamos
+      gamesList.map((vg) => {
+        videogames.push({
+          id: vg.id,
+          name: vg.name,
+          image: vg.background_image,
+          released: vg.released,
+          rating: vg.rating,
+          platforms: vg.platforms?.map((el) => el.platform.name),
+          genres: vg.genres?.map((el) => el.name),
+        });
       });
-    });
+      // Pasar a la siguiente página
+      url = response.data.next;
+    }
+    return videogames;
+  } catch (error) {
+    console.log(error);
   }
-  console.log(videogames.length);
-  return videogames;
 };
 
-module.exports = { apiData };
+// ENVIAR LOS 100 JUEGOS A LA BASE DE DATOS
+// llamado asincrono a la base de datos
+const dbData = async () => {
+  try {
+    return await Videogame.findAll({
+      //SELECT * FROM Videogame
+      include: [
+        {
+          model: Genre,
+          atributes: ["name"],
+          throught: {
+            attributes: [],
+          },
+        },
+      ],
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}; // Devuelve un array vacio, ya que en nuestra bdd no tenemos nada
+
+module.exports = { apiData, dbData };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { apiData } = require("../controllers");
+const { apiData, dbData } = require("../controllers");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,8 +9,12 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 router.get("/videogames", async (req, res) => {
   // GUARDO LOS DATOS DE LO QUE HAY EN TODA LA API
-  const apiGames = await apiData();
-  res.send(apiGames);
+  const apiGames = await apiData(); // [100 games]
+
+  const dbGames = await dbData(); // []
+
+  const finalData = [...apiGames, ...dbGames];
+  res.json(finalData);
 });
 
 module.exports = router;

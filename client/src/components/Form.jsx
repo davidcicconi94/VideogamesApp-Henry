4d7046db1e61206f072971e1953c150fb04b9b01
David <david.cicconi94@gmail.com>
2022-08-29 01:10:57 -0300

    platform input OK

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getbyGenres } from "../redux/actions/actions";
+import { getbyGenres, getPlatforms } from "../redux/actions/actions";
 
 const Form = () => {
   const navigate = useNavigate();
   const genres = useSelector((state) => state.genres);
+  const platforms = useSelector((state) => state.platforms);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getbyGenres());
+    dispatch(getPlatforms());
   }, [dispatch]);
 
   return (
@@ -41,11 +44,23 @@ const Form = () => {
         <p>
           <label>Genres:</label>
           <select name="genres" id="genres">
-            <option value="" disabled>
-              Choose your genre...
+            <option disabled selected>
+              ---
             </option>
             {genres.map((gen) => {
               return <option> {gen.name} </option>;
+            })}
+          </select>
+        </p>
+
+        <p>
+          <label>Platforms:</label>
+          <select name="platforms" id="platforms">
+            <option disabled selected>
+              ---
+            </option>
+            {platforms.map((plat) => {
+              return <option> {plat} </option>;
             })}
           </select>
         </p>

cf4d1d2b1b02738ef4b2610162f39069d0521dbf
David <david.cicconi94@gmail.com>
2022-08-31 12:07:07 -0300

    model tests ok

--- api/tests/models/videogame.spec.js
+++ api/tests/models/videogame.spec.js
@@ -1,21 +1,28 @@
-const { Videogame, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const { Videogame, conn } = require("../../src/db.js");
+const { expect } = require("chai");
 
-describe('Videogame model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
+describe("Videogame model", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+
+  let game = {
+    name: "Resident Evil 4",
+    platforms: ["PC", "PS2"],
+  };
+
+  describe("Validators", () => {
     beforeEach(() => Videogame.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
+    describe("name", () => {
+      it("should throw an error if name is null", (done) => {
         Videogame.create({})
-          .then(() => done(new Error('It requires a valid name')))
+          .then(() => done(new Error("It requires a valid name")))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Super Mario Bros' });
+      it("should work when its a valid name", () => {
+        Videogame.create({ name: game.name });
       });
     });
   });
